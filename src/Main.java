37aef29f38955354a9b21ce58fd41a15737a2b6a
nadsay <nadsatyyy@gmail.com>
2023-09-02 15:21:18 +0300

    Домашка 10, Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -12,6 +13,18 @@ public class Main {
         String fullName = lastName + " " + firstName + " " + middleName;
 
         System.out.println("ФИО сотрудника - " + fullName);
+
+    }
+
+    public static void task2() {
+        System.out.println("\n Задача 2 \n");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase ());
 
     }
 }
